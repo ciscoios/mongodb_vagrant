9d9f83ba9dae78a991aa04971dc9e749588627ae
Petro Bogdan <you@example.com>
2021-01-24 20:49:39 +0200

    Refacted vagrant+mongodb lab

--- Vagrantfile
+++ Vagrantfile
@@ -1,33 +1,20 @@
 Vagrant.configure("2") do |config|
 
-#debian9
-#  config.vm.box = "debian/stretch64"
+  config.vm.box = "ubuntu/hirsute64"
+  config.vm.hostname = "mongodb-lab"
+  #config.vm.network :private_network, ip: "192.168.56.2", guest: 27017, host: 27017
+  #config.vm.network "forwarded_port",  host_ip: "127.0.0.1", guest: 27017, host: 27017
+config.vm.network "forwarded_port", host_ip: "127.0.0.1", guest_ip: "127.0.0.1", guest: 27017, host: 27017,
+    auto_correct: true
 
-#debian8
-config.vm.box = "debian/jessie64"
-
-  config.vm.hostname = "mongodb-test"
-  config.vm.network :private_network, ip: "192.168.56.2", guest: 27017, host: 27017
-  # config.vm.network "forwarded_port", guest: 27017, host: 27017
-#   config.vm.network "forwarded_port", guest: 27017, host: 27018
-  #  "forwarded_port", guest: 2003, host: 27017, protocol: "tcp"
- #config.vm.forward_port 27017, 27018
-
-  # ssh settings
-  config.ssh.insert_key = false
-  config.ssh.private_key_path = ["keys/private/private.ppk", "~/.vagrant.d/insecure_private_key"]
-#  config.vm.provision "file", source: "keys/public", destination: "~/.ssh/authorized_keys"
-  config.vm.provision "shell", inline: <<-EOC
-#    sudo sed -i -e "\\#PasswordAuthentication yes# s#PasswordAuthentication yes#PasswordAuthentication no#g" /etc/ssh/sshd_config
-    sudo service ssh restart
-	#mkdir /vagrant-data
-  EOC
- 
-#config.vm.synced_folder "data", "/vagrant-data", type: "nfs" 
- 
 #to change vm name in Virtualbox
    config.vm.provider "virtualbox" do |v|
-  v.name = "mongodb-test"
+ # Do not display the VirtualBox GUI when booting the machine
+  v.gui = false
+  v.name = "mongodb-lab"
+  # Customize the amount of memory on the VM:
+  v.memory = 2048
+
 end
 config.vm.provision "shell",
     path: "mongodb_install.sh"
--- mongodb_install.sh
+++ mongodb_install.sh
@@ -1,59 +1,49 @@
 #!/bin/bash
 
-#install vim
-sudo apt-get -y install vim && \
+#Install gpg key
+sudo apt-get install -y gnupg 
 
-#add ssh-rsa key
-#echo ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAp5k+1UT8DTb35qM7lXeuZcLs1NME0Zm9wAfioBoGSr1BUGvjMtq/A4tInLCfN+HI8Kx3HXlNIAJXaOjPc2EPPgwN2LMYR184HjCYydhTDb8UW3riORNL+3MjgAL5OgYNS9zsFtmn3utCT+B4Lo4itXyGNwLoFqy6zJ2XpKiow3GY/A3gPcBA8WGQUbiFcXqW2yM0Cac2gynhG1Vb8CdqYQNv95x3z9WSId3RIc5F1TYMa5kCxAh57Aaz3us/SmW3RZ4tQULXI2Wo8i6WydXNji44+I/BOPmwuGoeg4vzY25b4RLPcf/dTWywdRcTiGLEoU4WePrqfWPqP2SqQqNicQ== rsa-key-20180115 > /home/vagrant/.ssh/authorized_keys && \
-#echo ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAmq81JfPP+SWsvjcz9p4CbCuqZ9Z/zIhN+pyFemygXoZ7yLeYnjdPVjhzEQl/ha2+bzel+GxK3Y8KJRxrG1YBfrqAoN1jcAvXuKFekaLa1Rd++TDsRQYMHhZWOrgOYHoFpfZLyZoUHoRQCmVL5YHcfXGldu/HsR/l4+nPSTYqXfdJPQTGXkdGELxqoaJxxyr2mrzH9AI8eGQS9vdtpkiHXBLaFp4YJRTTHSe45iw+btGeknNGl355BdPAe+JSinhq/3DkdZwqCQ6tpemwc0ik4F4znmooDsmzsdfl4Cb8ccymEh1E/BdLs8jw7zmj+Kay6xuLU79cBOvB5e/Vub5Enw== rsa-key-20180116 > /home/vagrant/.ssh/authorized_keys && \
-#cp id_rsa.pub > ~/.ssh/id_rsa
+#Import the public key used by the package management system
+wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo gpg --no-default-keyring --keyring gnupg-ring:/etc/apt/trusted.gpg.d/mongodb.gpg --import
 
-#Import key from the official MongoDB repository.
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6 && \
+#Setup read/write permissions to our newly created gpg key
+sudo chmod 644 /etc/apt/trusted.gpg.d/mongodb.gpg
 
-#Issue the following command to create a list file for MongoDB.
-echo "deb http://repo.mongodb.org/apt/debian jessie/mongodb-org/3.4 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list && \
 
-#update the packages list
-sudo apt-get update && \
+#Create a list file for MongoDB
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
 
-#install the MongoDB package itself with the following command:
-sudo apt-get install -y mongodb-org && \
+#Reload local package database.
+sudo apt-get update
 
-#start the service, and ensure it starts when your server reboots:
-sudo systemctl enable mongod.service && \
-sudo systemctl start mongod && \
+#Install mongodb
+sudo apt-get install -y mongodb-org
 
-#check that the service has started properly
-sudo systemctl status mongod | grep -i 'active(running)' |echo $? | if [ $? == 0 ]; \
-then \
-#to make possibility click save in non-interactive mode
-echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections &&  \
-echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections &&  \
-#echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections
-#for ansible
-#- name: prevent the iptables-peristent install dialog
-#  debconf: name=iptables-persistent question={{ item }} vtype=boolean value=true
-#  with_items:
-#  - iptables-persistent/autosave_v4
-#  - iptables-persistent/autosave_v6
-#- name: install iptables-persistent
-#  apt: name=iptables-persistent
+#Start mongodb
+sudo systemctl start mongod || sudo systemctl daemon-reload
 
-sudo apt-get install -y iptables-persistent && \
-#Next, remove any existing rules that may be in place, just in case:
-sudo iptables -F && \
-sudo iptables -P INPUT DROP && \
-#sudo iptables -P FORWARD ACCEPT && \
-#sudo iptables -P OUTPUT ACCEPT && \ 
-sudo iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT && \
-sudo iptables -A INPUT -p tcp -m tcp --dport 22 -j ACCEPT && \
-sudo iptables -A INPUT -s 192.168.56.0/24 -p tcp -m tcp --dport 27017 -m state --state NEW,ESTABLISHED -j ACCEPT && \
-sudo iptables -A INPUT -i lo -j ACCEPT && \
-sudo iptables -A OUTPUT -d 192.168.56.2/32 -p tcp -m tcp --sport 27017 -m state --state ESTABLISHED -j ACCEPT && \
-sudo iptables -A OUTPUT -o lo -j ACCEPT && \
-#sudo netfilter-persistent save && \
-#sudo iptables-save /etc/iptables.save && \
-perl -pi -e 's/bindIp: 127.0.0.1/#bind_ip=127.0.0.1/g' /etc/mongod.conf \
-; else exit 0; fi
+#Enable autoload after restart
+sudo systemctl enable mongod
 
+#Setup initial delay to let Mongodb enough time to startup 
+sleep 10
+
+#check if database with collections is exist(will return -1), if not create database and user
+
+if [ $(mongo localhost:27017 --eval 'db.getMongo().getDBNames().indexOf("spark")' --quiet) -lt 0 ]; then
+    echo "Creating database with collections and create database user"
+# create database with collections users
+    mongo 127.0.0.1:27017 --eval "db=db.getSiblingDB('spark'); db.createCollection('users');"
+   # Add user to database
+    mongo 127.0.0.1:27017 --eval "db=db.getSiblingDB('spark'); db.createUser({ user: 'iptcp', pwd: 'root', roles: [ {role: 'readWrite', db: 'spark'}]});"
+
+else
+    echo "Database with collections exist, exiting"
+    exit 0
+fi
+
+#List created spark database
+mongo 127.0.0.1:27017 --eval 'db.adminCommand( { listDatabases: 1, filter: { "name": /^spark/ } } )'
+
+#List created admin user
+mongo 127.0.0.1:27017 --eval "db.getSiblingDB('spark').getUsers({ filter: { "user": /^iptcp/ } })"
