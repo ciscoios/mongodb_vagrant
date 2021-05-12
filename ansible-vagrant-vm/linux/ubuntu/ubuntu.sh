4bd0fe4b5e812cd4b89d5e4fadb81b4f47c48066
Petro Bogdan <you@example.com>
2021-05-12 15:48:15 +0300

    Splited config for windows and linux hosts and added ssh login part from host

---
+++ ansible-vagrant-vm/linux/ubuntu/ubuntu.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo apt-get update && sudo DEBIAN_FRONTEND=noninteractive apt-get install -y openssh-client
---
+++ ansible-vagrant-vm/windows/centos/centos.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo yum -y update && sudo yum -y install openssh-clients python38
